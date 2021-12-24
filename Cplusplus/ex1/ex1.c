677d562233e0489c3236a89ce85c830f2daf118b
saulczdl@gmail.com <saulczdl@gmail.com>
2021-12-23 21:45:15 -0300

    Christmas

--- Cplusplus/ex1/ex1.c
+++ Cplusplus/ex1/ex1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /* This is a comment. */
-//int main(int argc, char *arqv[])
-int main(void)
+int main(int argc, char *arqv[])
+//int main(void)
 {
-	int distance = 100;
+	int distance = 150;
 
 	//this is also a comment
 	printf("You are %d miles away.\n", distance);
 	printf("I have %d dolar.\n", distance);
-	printf("Hello World %d times.\n", distance);
-	printf("Saul, \n");
+	printf("Hello World %d times.\n"
+		"My name is Saul Calzadilla, but everybory calls me 'Saul'...\n",
+	       distance);
+	printf("KKKKKKkk,%d times, %d!!! \n", distance, distance);
 
 	return 0;
 }
